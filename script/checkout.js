f3873f4331b4384ad025b477816cb2a15c03885c
Onyenso Precious <chinoprecious29@gmail.com>
2024-01-25 03:47:21 -0800

    delete product from cart which clicked delete

--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 
 import {product} from '../data/products.js';
 
@@ -46,7 +46,8 @@ let cartSummaryHTML = '';
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" 
+          data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -103,3 +104,12 @@ let cartSummaryHTML = '';
 
 
 document.querySelector('.js-cart-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () =>{
+    const productId = link.dataset.productId;
+
+    removeFromCart(productId);
+    console.log(cart);
+  } )
+})
