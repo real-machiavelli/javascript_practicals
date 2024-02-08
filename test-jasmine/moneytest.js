65f3fc7ee5376a17ae545293c08def89d07ee4aa
Onyenso Precious <chinoprecious29@gmail.com>
2024-02-08 05:27:23 -0800

    add money test

---
+++ test-jasmine/moneytest.js
@@ -0,0 +1,18 @@
+import {formatCurrency} from '../script/util/money.js';
+
+
+  describe('test suite: formatcurrency', () =>{
+
+    it('concerts cents into doller', () => {
+      expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('work with 0', () => {
+      expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('round up to the nearest cents', () => {
+      expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+
+  });
